cf487b95496236d836ba345a0f45e423c0312e18
Gianmarco Folchi <gmfolc@gmail.com>
2021-07-12 03:53:59 -0400

    Added SQLAlchemy Functionality and  updated  the get, put and delete methods

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
-#resqparse in not accesed
+from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
 names = {}
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+class NameModel(db.Model): 
+    name = db.Column(db.String(20), primary_key = True)
+    age = db.Column(db.Integer, nullable = False)
+    email = db.Column(db.String(50), nullable = False)
+    
 
 namesPutArgs = reqparse.RequestParser() 
-namesPutArgs.add_argument("age", type = int, help = "Age of person") 
-namesPutArgs.add_argument("gender", type = str, help = "gender of person") 
-namesPutArgs.add_argument("email", type = str, help = "email of person") 
+namesPutArgs.add_argument("age", type = int, help = "Age of person is required", required = True) 
+namesPutArgs.add_argument("gender", type = str, help = "gender of person is required", required = True) 
+namesPutArgs.add_argument("email", type = str, help = "email of person is required", required = True) 
+
+def abortIfNameDNE(name):
+    if name not in names: 
+        abort(404, message = "Name not found")
+        
+def abortIfNameExists(name): 
+    if name in names: 
+        abort(409, message = "A person with that name already exists")
 
 class HelloWorld(Resource): 
-    # def get(self, name):  
-    #     return names[name]
+    def get(self, name):  
+        abortIfNameDNE(name)
+        return names[name]
     
     def put(self, name): 
+        abortIfNameExists(name)
         args = namesPutArgs.parse_args()
-        return {name: args}
+        names[name] = args
+        return names[name], 201
+    
+    def delete(self, name):
+        abortIfNameDNE(name)
+        del names[name]
+        return "", 204
+        
 
 api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
-response = requests.put(BASE + "helloworld/jeff", {"age": 15, })
+print("Put: ")
+response = requests.put(BASE + "helloworld/Nicolle", {"age": 15, "gender": "female", "email": "nicolle.folchi@hotmail.com"})
+print(response.json())   
+input()
+print("Get: ")
+response = requests.get(BASE + "helloworld/jeff")
 print(response.json())   
